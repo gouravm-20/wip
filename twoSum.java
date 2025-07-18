81279524bb284600799a678d42bad3117b258aad
gouravm-20 <76433371+gouravm-20@users.noreply.github.com>
2025-07-18 09:44:30 +0530

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -14,3 +14,18 @@ class Solution {
         return indices;        
     }
 }
+
+// Using HashMap: Time complexity - O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        for(int idx=0; idx<nums.length; idx++) {
+            int y = target-nums[idx];
+            if(hm.containsKey(y)) {
+                return new int[] {hm.get(y), idx};
+            }
+            hm.put(nums[idx], idx);
+        }
+        return new int[] {0};
+    }
+}
